89588a373936c1908ed78308281d8868560f20ae
rajacsp <raja.csp@gmail.com>
2017-11-05 13:42:54 -0500

    Pom clean up

--- src/main/java/com/packtpub/restapp/ticketmanagement/TicketManagementApplication.java
+++ src/main/java/com/packtpub/restapp/ticketmanagement/TicketManagementApplication.java
@@ -1,23 +1,36 @@
 package com.packtpub.restapp.ticketmanagement;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-@Controller
+@RestController
 public class TicketManagementApplication {
 	
 	@ResponseBody
 	@RequestMapping("/")
 	public String sayAloha(){
 		return "Aloha";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/test")
+	public Map<String, Object> test(){
+		Map<String, Object> map = new LinkedHashMap<>();
+		map.put("one", "two");
+		map.put("three", "four");
+		
+		return map;
 	}
 
 	public static void main(String[] args) {
